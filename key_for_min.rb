f876d8af4561d81efdb80d4eff41ee1845c14aa6
yehudis raitman <yehudisr@gmail.com>
2021-01-20 16:01:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,12 @@
 def key_for_min_value(name_hash)
 
 name_hash.each do |key, value|
-  min_value = 150
+  min_value = 100000
+  key_to_store = nil
   if value < min_value  
   min_value = value
-  key_to_store = key
+  
   end
- name_hash[key_to_store]
+ key_to_store
 end
 end
